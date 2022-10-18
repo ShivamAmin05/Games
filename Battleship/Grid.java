e9edab82fc902170f112d1981f52a88f71740a0e
ShivamAmin05 <shivamamin04@gmail.com>
2022-10-18 12:14:43 -0400

    updated grid to print with row and char headers

--- Battleship/Grid.java
+++ Battleship/Grid.java
@@ -60,18 +60,21 @@ public class Grid {
         }
     }
     public void printGrid()
-    {
-        System.out.println("┏———————————————————┓");
+    {   System.out.println(" 1 2 3 4 5 6 7 8 9 10");
+        System.out.println(" ┏———————————————————┓");
+        int rowChar = 65;
         for(int i = 0; i < 10; i++)
         {
+            System.out.print((char)(rowChar));
+            rowChar ++;
             System.out.print("┃");
             for(int j = 0; j < 10; j++)
             {
                 System.out.print(grid[i][j] + "┃");
             }
-            System.out.println("\n┃—┃—┃—┃—┃—┃—┃—┃—┃—┃—┃");
+            System.out.println("\n ┃—┃—┃—┃—┃—┃—┃—┃—┃—┃—┃");
             
         }
-        System.out.print("┗———————————————————┛");
+        System.out.print(" ┗———————————————————┛");
     }
 }
